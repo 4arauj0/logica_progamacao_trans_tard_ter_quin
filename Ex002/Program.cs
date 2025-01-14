8c6ed0cc0aea3a15702a2370fcfab2a17deaf906
FELIPE ARAUJO DE OLIVEIRA <felipe.aoliveira54@senacsp.edu.br>
2025-01-14 15:24:12 -0300

    first commit~

---
+++ Ex002/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ola sou araujo");
+Console.WriteLine("estou aprendendo logica de progamaçao");
---
+++ ex003/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ola meu nome é araujo");
+Console.WriteLine("estou estou estudando no senac largo 13");
